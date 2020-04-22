b1a2fb8d982ca10721958f6e5a4c95ab0272bd77
Garvin <gming27@gmail.com>
2020-04-22 11:13:31 -0400

    added "server:debug" script for nodemon inspector, console logging app and get for inspection testing

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys_dev').mongoURI;
 
+console.log("app:", app);
+const appGet = app.get;
+console.log("app.get:", appGet);
+
 mongoose
   .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hey World!"));
+app.get("/", (req, res) =>{
+  console.log(res)
+  res.send("Hi World!");
+});
 
 const port = process.env.PORT || 5000;
 
