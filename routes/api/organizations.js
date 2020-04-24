5218a296ae02605e197360562f8fefc87bb43158
Garvin <gming27@gmail.com>
2020-04-23 23:23:05 -0400

    creates models folder/Organization Model/ Organization Schema

--- routes/api/organizations.js
+++ routes/api/organizations.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const org = express.Router();
+const validateRegister = require('../../validation/register');
+const validateLogin = require('../../validation/login');
 
 org.get('/test', (req, res) => {
   res.json ({ msg: "This is the organization route"});
