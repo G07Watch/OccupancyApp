7383f306958839d82a088c9503f05b7cfb101d92
Garvin <gming27@gmail.com>
2020-05-01 11:55:26 -0400

    added reducers folder with file frames for entities, errors, organization, root and session reducers

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,14 @@
+
+
+const initialState ={
+  isAuthenticated: false,
+  organization: {}
+}
+
+export default (state = initialState, action) =>{
+
+  switch(action.type){
+
+    default: return state;
+  }
+}
