b53ec27b4ce512f1f48707a9ee61d1975bb1973d
Garvin <gming27@gmail.com>
2020-05-01 13:00:54 -0400

    adds cases to session reducer

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,14 +1,33 @@
+import merge from 'lodash/merge';
 
+import {
+  RECEIVE_CURRENT_ORG,
+  RECEIVE_ORG_LOGOUT,
+  RECEIVE_ORG_SIGN_IN
+} from '../actions/session_actions';
 
 const initialState ={
   isAuthenticated: false,
-  organization: {}
+  org: {}
 }
 
 export default (state = initialState, action) =>{
-
   switch(action.type){
-
-    default: return state;
+    case RECEIVE_CURRENT_ORG:
+      return merge({}, state, {
+        isAuthenticated: !!action.currentOrg,
+        org: action.currentOrg
+      });
+    case RECEIVE_ORG_LOGOUT:
+      return {
+        isAuthenticated: false,
+        org: undefined
+      };
+    case RECEIVE_ORG_SIGN_IN:
+      return merge({}, state, {
+        isAuthenticated: true
+      });
+    default: 
+      return state;
   }
 }
